fdfad3aa3a22e99e5c22571ff9e925e54597d78d
Fanomezantsoa-Dévellopeur <fanomezanaoriginal@gmail.com>
2026-06-18 20:45:36 +0300

    fix: recreate app.module.ts cleanly with all modules

--- backend_bloc_operatoire/dist/app.module.js
+++ backend_bloc_operatoire/dist/app.module.js
@@ -7,9 +7,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
-const notification_cpa_module_1 = require("./notification-cpa/notification-cpa.module");
-const prescription_module_1 = require("./prescription/prescription.module");
-const webhook_notification_module_1 = require("./webhook-notification/webhook-notification.module");
 const common_1 = require("@nestjs/common");
 const config_1 = require("@nestjs/config");
 const typeorm_1 = require("@nestjs/typeorm");
@@ -24,6 +21,7 @@ const activite_per_op_module_1 = require("./activite-per-op/activite-per-op.modu
 const protocole_operatoire_module_1 = require("./protocole-operatoire/protocole-operatoire.module");
 const score_sccre_module_1 = require("./score-sccre/score-sccre.module");
 const sortie_reveil_module_1 = require("./sortie-reveil/sortie-reveil.module");
+const notification_cpa_module_1 = require("./notification-cpa/notification-cpa.module");
 const auth_module_1 = require("./auth/auth.module");
 const archives_module_1 = require("./archives/archives.module");
 const rapports_module_1 = require("./rapports/rapports.module");
@@ -33,20 +31,17 @@ const exports_module_1 = require("./exports/exports.module");
 const checklist_avant_op_module_1 = require("./checklist-avant-op/checklist-avant-op.module");
 const checklist_pendant_op_module_1 = require("./checklist-pendant-op/checklist-pendant-op.module");
 const checklist_apres_op_module_1 = require("./checklist-apres-op/checklist-apres-op.module");
+const webhook_notification_module_1 = require("./webhook-notification/webhook-notification.module");
+const prescription_module_1 = require("./prescription/prescription.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
         imports: [
-            prescription_module_1.PrescriptionModule,
-            webhook_notification_module_1.WebhookNotificationModule,
             config_1.ConfigModule.forRoot({ isGlobal: true }),
             typeorm_1.TypeOrmModule.forRootAsync({
-                imports: [
-                    prescription_module_1.PrescriptionModule,
-                    webhook_notification_module_1.WebhookNotificationModule, config_1.ConfigModule
-                ],
+                imports: [config_1.ConfigModule],
                 inject: [config_1.ConfigService],
                 useFactory: (config) => ({
                     type: 'postgres',
@@ -56,12 +51,27 @@ exports.AppModule = AppModule = __decorate([
                     synchronize: false,
                 }),
             }),
-            patient_module_1.PatientModule, medecin_module_1.MedecinModule, cpa_module_1.CPAModule, vpa_module_1.VPAModule, bon_commande_module_1.BonCommandeModule,
-            activite_per_op_module_1.ActivitePerOpModule, protocole_operatoire_module_1.ProtocoleOperatoireModule,
-            score_sccre_module_1.ScoreSCCREModule, sortie_reveil_module_1.SortieReveilModule, notification_cpa_module_1.NotificationCPAModule,
-            auth_module_1.AuthModule, archives_module_1.ArchivesModule, rapports_module_1.RapportsModule, planning_module_1.PlanningModule,
-            tracabilite_module_1.TracabiliteModule, exports_module_1.ExportsModule,
-            checklist_avant_op_module_1.ChecklistAvantOpModule, checklist_pendant_op_module_1.ChecklistPendantOpModule, checklist_apres_op_module_1.ChecklistApresOpModule,
+            patient_module_1.PatientModule,
+            medecin_module_1.MedecinModule,
+            cpa_module_1.CPAModule,
+            vpa_module_1.VPAModule,
+            bon_commande_module_1.BonCommandeModule,
+            activite_per_op_module_1.ActivitePerOpModule,
+            protocole_operatoire_module_1.ProtocoleOperatoireModule,
+            score_sccre_module_1.ScoreSCCREModule,
+            sortie_reveil_module_1.SortieReveilModule,
+            notification_cpa_module_1.NotificationCPAModule,
+            auth_module_1.AuthModule,
+            archives_module_1.ArchivesModule,
+            rapports_module_1.RapportsModule,
+            planning_module_1.PlanningModule,
+            tracabilite_module_1.TracabiliteModule,
+            exports_module_1.ExportsModule,
+            checklist_avant_op_module_1.ChecklistAvantOpModule,
+            checklist_pendant_op_module_1.ChecklistPendantOpModule,
+            checklist_apres_op_module_1.ChecklistApresOpModule,
+            webhook_notification_module_1.WebhookNotificationModule,
+            prescription_module_1.PrescriptionModule,
         ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
